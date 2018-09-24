05a477bda21c562d97cd394b7fd73a61f549008f
Johannes Lund <johannes.lund@iohk.io>
2018-09-24 12:52:29 +0200

    [CO-373] Copy Mnemonic.hs to wallet-new as BIP39.hs
    Also move MnemonicSpec.hs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -18159,6 +18159,7 @@ license = stdenv.lib.licenses.mit;
 , async
 , base
 , base58-bytestring
+, basement
 , beam-core
 , beam-migrate
 , beam-sqlite
@@ -18286,6 +18287,7 @@ aeson-pretty
 async
 base
 base58-bytestring
+basement
 beam-core
 beam-migrate
 beam-sqlite
