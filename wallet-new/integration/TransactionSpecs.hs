ac64fbb44b2fb44274a724c150e163eb0fcd68b7
MarcFontaine <marc.fontaine@iohk.io>
2018-10-23 14:52:06 +0200

    [CO-424] Add integration tests for Cardano Hardware Wallets
    * Add the module HardwareWalletSpecs.
      This module exports HSpec tests for the following endpoints :
         postUnsignedTransaction  -- retrieve an unsigned transaction from the wallet
         postSignedTransaction    -- post an externally signed transaction
      The tests take a seed::Integer as argument.
      This seed is used to create new unique wallets as needed.
    
    * Add unsafeProtocolMagic as a new top level declaration to Main.
      Hard-coding ProtocolMagic in Main is a hack that should be fixed !
      ProtocolMagic is needed in the tests to compute crypto function (simulating an external
      ledger device).
      The ProtocolMagic should be passed in from the CLI or read from the defining config file.

--- wallet-new/integration/TransactionSpecs.hs
+++ wallet-new/integration/TransactionSpecs.hs
@@ -11,7 +11,8 @@ import           Test.Hspec
 import           Test.QuickCheck.Monadic (PropertyM, run)
 import           Text.Show.Pretty (ppShow)
 
-import           Util
+import           Util hiding (makePayment)
+import qualified Util
 
 import qualified Data.Map.Strict as Map
 import qualified Pos.Chain.Txp as Txp
@@ -232,25 +233,8 @@ transactionSpecs wRef wc = beforeAll_ (setupLogging "wallet-new_transactionSpecs
             liftIO $ utxoStatistics `shouldBe` utxoStatisticsExpected
 
   where
-    makePayment
-        :: Core.Coin
-        -> (Wallet, Account)
-        -> Core.Address
-        -> IO (Either ClientError Transaction)
-    makePayment amount (sourceW, sourceA) destination = do
-        let payment = Payment
-                { pmtSource = PaymentSource
-                    { psWalletId     = walId sourceW
-                    , psAccountIndex = accIndex sourceA
-                    }
-                , pmtDestinations = pure PaymentDistribution
-                    { pdAddress = V1 destination
-                    , pdAmount  = V1 amount
-                    }
-                , pmtGroupingPolicy   = Nothing
-                , pmtSpendingPassword = Nothing
-                }
-        fmap (fmap wrData) $ postTransaction wc payment
+    makePayment amount (sourceW, sourceA) destination
+       = fmap (fmap wrData) $ Util.makePayment wc amount (sourceW, sourceA) destination
 
     getRandomAddress
         :: IO Core.Address
